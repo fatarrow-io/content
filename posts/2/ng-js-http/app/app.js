6c2258eb1d489c83c31115d1cda7657d3b75bccc
Zach <briggszj@gmail.com>
2015-07-13 21:35:44 -0500

    Start post 2

---
+++ posts/2/ng-js-http/app/app.js
@@ -0,0 +1,11 @@
+function AppComponent() {
+}
+
+AppComponent.annotations = [
+  new ng.ComponentAnnotation({
+    selector: 'app',
+  }),
+  new ng.ViewAnnotation({
+    template: '<h1>HTTP</h1>',
+  })
+]
---
+++ posts/2/ng-js-http/index.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+  ng.bootstrap(AppComponent, [])
+})
