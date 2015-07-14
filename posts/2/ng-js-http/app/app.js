ea92e2ee254a03f9d3a987a2346c7bea23c355c1
Zach <briggszj@gmail.com>
2015-07-13 22:37:01 -0500

    First successfull request

--- posts/2/ng-js-http/app/app.js
+++ posts/2/ng-js-http/app/app.js
@@ -1,9 +1,12 @@
-function AppComponent() {
+function AppComponent(http) {
+  console.log(http.get('https://api.github.com/'))
 }
 
+AppComponent.parameters= [[ng.Http]]
 AppComponent.annotations = [
   new ng.ComponentAnnotation({
     selector: 'app',
+    injectables: [ng.Http]
   }),
   new ng.ViewAnnotation({
     template: '<h1>HTTP</h1>',
--- posts/2/ng-js-http/index.js
+++ posts/2/ng-js-http/index.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  ng.bootstrap(AppComponent, [])
+  ng.bootstrap(AppComponent, [
+    ng.httpInjectables
+  ])
 })
